d94da2efa95fad5faa7558353cda3435c79abc8e
BombShadow958 <158053061+BombShadow958@users.noreply.github.com>
2024-07-24 10:17:14 -0300

    Enemy Goes Home

--- Box_Game/Assets/EnemyScript.cs
+++ Box_Game/Assets/EnemyScript.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class EnemyScript : MonoBehaviour
 {
     public float m_speed;
-    public float m_LineOfSite;
+    public float m_AggroRange;
 
     private Transform player;
+
+    [SerializeField] Transform m_CastPoint;
+
+    bool isPatrolling;
+    bool isHome;
+
+    Vector2 m_HomePoint;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        m_HomePoint = transform.position;
         player = GameObject.FindGameObjectWithTag("Player").transform;    
     }
 
@@ -20,15 +28,74 @@ public class EnemyScript : MonoBehaviour
     void Update()
     {
         float distanceFromPlayer = Vector2.Distance(player.position, transform.position);
-        if (distanceFromPlayer < m_LineOfSite)
+        if (distanceFromPlayer < m_AggroRange)
         {
-            transform.position = Vector2.MoveTowards(this.transform.position, player.position, m_speed * Time.deltaTime);
+            ChasePlayer();
+        } else
+        {
+            Patrol();
+        }
+    }
+
+    bool CanSeePlayer(float distance)
+    {
+        bool val = false;
+        var mCastDist = distance;
+
+        Vector2 mEndPos = m_CastPoint.position + Vector3.right * distance;
+
+        RaycastHit2D hit = Physics2D.Linecast(m_CastPoint.position, mEndPos, 1 << LayerMask.NameToLayer("Action"));
+
+        if (hit.collider != null)
+        {
+            if (hit.collider.gameObject.CompareTag("Player"))
+            {
+                //Aggro sees enemy
+                val = true;
+            }
+            else
+            {
+                val = false;
+            }
+            Debug.DrawLine(m_CastPoint.position, mEndPos, Color.blue);
+        }
+        return val;
+    }
+
+    private void ChasePlayer()
+    {
+        isPatrolling = false;
+        isHome = false;
+        transform.position = Vector2.MoveTowards(this.transform.position, player.position, m_speed * Time.deltaTime);
+    }
+
+    private void Patrol()
+    {
+        if (!isPatrolling)
+        {
+            ReturnHome();
+        } else
+        {
+
+        }
+    }
+
+    private void ReturnHome()
+    {
+        if (isHome)
+        {
+            isPatrolling = true;
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, m_HomePoint, m_speed * Time.deltaTime);
+            //transform.position = m_HomePoint;
         }
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(transform.position,m_LineOfSite);
+        Gizmos.DrawWireSphere(transform.position,m_AggroRange);
     }
 }
