6befae74f2e74d18d6334594485c417e47960b7a
BombShadow958 <158053061+BombShadow958@users.noreply.github.com>
2024-07-25 15:00:00 -0300

    Boss with gun

--- Box_Game/Assets/Scripts/Enemies/BossScript.cs
+++ Box_Game/Assets/Scripts/Enemies/BossScript.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class BossScript : MonoBehaviour
 {
+    public float m_speed;
     public float m_AggroRange;
+    public float m_ShootingRange;
+    public float m_FireRate = 1.0f;
+    private float m_nextFireTime;
+    public GameObject m_bullet;
+    public GameObject m_bulletParent;
 
     private Transform player;
 
@@ -15,19 +21,23 @@ public class BossScript : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         float distanceFromPlayer = Vector2.Distance(player.position, transform.position);
-        if (distanceFromPlayer < m_AggroRange)
+
+        if (distanceFromPlayer < m_AggroRange && distanceFromPlayer > m_ShootingRange)
         {
-            
+            transform.position = Vector2.MoveTowards(this.transform.position, player.position, m_speed * Time.deltaTime);
+
         }
-        else
+        else if (distanceFromPlayer <= m_ShootingRange && m_nextFireTime < Time.time)
         {
-            
+            Instantiate(m_bullet,m_bulletParent.transform.position, Quaternion.identity);
+            m_nextFireTime = Time.time + m_FireRate;
         }
     }
 
@@ -35,5 +45,6 @@ public class BossScript : MonoBehaviour
     {
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position, m_AggroRange);
+        Gizmos.DrawWireSphere(transform.position, m_ShootingRange);
     }
-    }
+}
